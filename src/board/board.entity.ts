d45572ad428db16f66c5fc39ae624db97f597496
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-09-10 19:45:56 +0900

    feat: add Board Entity

--- src/board/board.entity.ts
+++ src/board/board.entity.ts
@@ -1,3 +1,19 @@
-import { Entity } from "typeorm";
+import { User } from "src/user/user.entity";
+import { BaseEntity, Column, Entity, JoinColumn, PrimaryGeneratedColumn, Unique } from "typeorm";
 
-@Entity
+@Entity({name:'board'})
+@Unique(['board_id'])
+export class Board extends BaseEntity{
+
+    @PrimaryGeneratedColumn('uuid')
+    id:string;
+
+    @Column({ type:'varchar', length : 100, comment : 'username'})
+    title:string;
+
+    @Column({ type:'varchar', length : 300, comment : 'username'})
+    content:string;
+
+    @JoinColumn([User])
+    
+}
