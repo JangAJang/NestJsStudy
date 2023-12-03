8aeb4c253ab5fadc1e98378c50004902114e634a
JangAJang <janghee5395@gmail.com>
2023-12-03 15:34:16 +0900

    feat: team, member에 대한 서비스, 컨트롤러 추가

--- src/team/team.module.ts
+++ src/team/team.module.ts
@@ -1,10 +1,14 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { Team, TeamSchema } from "./entity/team";
+import { TeamService } from './team.service';
+import { TeamController } from './team.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Team.name, schema: TeamSchema }]),
   ],
+  providers: [TeamService],
+  controllers: [TeamController],
 })
 export class TeamModule {}
